27dbb047be765d11b2542e80ebf13f8967978c32
Alberto Pérez <alberto.perez.torres@est.fib.upc.edu>
2017-03-24 01:05:56 +0100

    Deformed triangle

--- MyGLWidget.cpp
+++ MyGLWidget.cpp
@@ -17,7 +17,7 @@ void MyGLWidget::initializeGL ()
 {
   // Cal inicialitzar l'ús de les funcions d'OpenGL
   initializeOpenGLFunctions();
-  
+
   glClearColor (0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
   carregaShaders();
   createBuffers();
@@ -27,12 +27,12 @@ void MyGLWidget::paintGL ()
 {
   glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
 
-  // Activem l'Array a pintar 
+  // Activem l'Array a pintar
   glBindVertexArray(VAO);
- 
+
   // Pintem l'escena
   glDrawArrays(GL_TRIANGLES, 0, 3);
-  
+
   // Desactivem el VAO
   glBindVertexArray(0);
 }
@@ -45,10 +45,11 @@ void MyGLWidget::resizeGL (int w, int h)
 void MyGLWidget::createBuffers ()
 {
   glm::vec3 Vertices[3];  // Tres vèrtexs amb X, Y i Z
-  Vertices[0] = glm::vec3(-1.0, -1.0, 0.0);
-  Vertices[1] = glm::vec3(1.0, -1.0, 0.0);
+  Vertices[0] = glm::vec3(-1.0, 0.0, 0.0);
+  Vertices[1] = glm::vec3(1.0, 0.0, 0.0);
   Vertices[2] = glm::vec3(0.0, 1.0, 0.0);
-  
+  //Vertices[3] = glm::vec3(0.0, 0.0, 0.0)
+
   // Creació del Vertex Array Object (VAO) que usarem per pintar
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
@@ -57,7 +58,7 @@ void MyGLWidget::createBuffers ()
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-  // Activem l'atribut que farem servir per vèrtex	
+  // Activem l'atribut que farem servir per vèrtex
   glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(vertexLoc);
 
